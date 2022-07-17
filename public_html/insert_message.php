6eeae50d2e3e2c6179e0ae2311362fe97bbecb5e
Vito Bodhisattva B. Subingsubing <qvbsubingsubing@tip.edu.ph>
2022-07-17 19:11:05 +0800

    Security Update: - Added RSA algorithms for encryption and decryption of certain features within the program - Item-related operations now get encrypted/decrpyted on the fly when user requests transactions related to them. - Messages are also now has encryption/decryption to secure sensitive information. - Changes are made to the admin page to integrate the security features of the applications. - Changed Date Order to Date In within the Out Items Table in the admin page. - Added error reporting features to fetch functions for easier debugging.

--- public_html/insert_message.php
+++ public_html/insert_message.php
@@ -2,13 +2,40 @@
 session_start();
 $sender_id = $_SESSION["curr_account_id"];
 $receiver_id = $_POST["message_receiver"];
-$message_content = $_POST["message"];
+$message_content = decrypt($_POST["message"]);
 
 // $servername = "localhost";
 // $username = "id18580145_presenteddatabaseusername";
 // $password = "^4v4<f]#Q)DU&{7R";
 // $dbname = "id18580145_presenteddatabasename";
+//RSA***
 
+function decrypt($sample_encrypted_message){
+  //DECRYPTING
+  $raw_decrypted_message = [];
+  $decrypted_message = "";
+  for($i = 0; $i < count($sample_encrypted_message) - 2; $i++){
+      $raw_decrypted_message[$i] = powerMod($sample_encrypted_message[$i], $sample_encrypted_message[count($sample_encrypted_message)-2], $sample_encrypted_message[count($sample_encrypted_message)-1]);
+  }
+  for($i = 0; $i < count($raw_decrypted_message); $i++){
+      $decrypted_message = $decrypted_message.chr($raw_decrypted_message[$i]);
+  }
+  return $decrypted_message;
+}
+function powerMod($base, $exponent, $modulus) {
+  if ($modulus === 1) return 0;
+  $result = 1;
+  $base = $base % $modulus;
+  while ($exponent > 0) {
+      if ($exponent % 2 === 1)  //odd number
+          $result = ($result * $base) % $modulus;
+      $exponent = $exponent >> 1; //divide by 2
+      $base = ($base * $base) % $modulus;
+  }
+  return $result;
+}
+
+//RSA***
 try {
   include ("database_warehouse_db.php");
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
